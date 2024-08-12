e0f1b8d09c5531a4c766ac104e585752592d680f
Tarique Iqbal <live.iqbal@gmail.com>
2024-08-12 16:08:47 +0200

    Implement ExceptionSubscriber

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onException',
+        ];
+    }
+
+    public function onException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $response = new Response();
+        $response->setContent($exception->getMessage());
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $event->setResponse($response);
+    }
+}
