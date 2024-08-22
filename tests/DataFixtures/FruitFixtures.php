279b4b86fc06ee7fe575324f15947f46ef93e5d8
Tarique Iqbal <live.iqbal@gmail.com>
2024-08-22 23:38:33 +0200

    Move data fixtures and tests

--- tests/DataFixtures/FruitFixtures.php
+++ tests/DataFixtures/FruitFixtures.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\DataFixtures;
+namespace App\Tests\DataFixtures;
 
 use App\Entity\Fruit;
 use Doctrine\Bundle\FixturesBundle\Fixture;
--- tests/FixtureTest/FruitTest.php
+++ tests/FixtureTest/FruitTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\FixtureTestCase;
+namespace App\Tests\FixtureTest;
 
 use App\Entity\Fruit;
 
