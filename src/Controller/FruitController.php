tarique-iqbal/fruits-and-vegetables

Implement FruitService

src/Controller/FruitController.php
@@ -4,51 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\Fruit;
-use App\Helper\PaginationHelper;
-use App\Repository\FruitRepository;
+use App\Service\FruitServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class FruitController extends AbstractController
+final class FruitController extends AbstractController
 {
     public function __construct(
-        private readonly FruitRepository $fruitRepository
+        private readonly FruitServiceInterface $fruitService,
     ) {
     }
 
     #[Route('/fruits', name: 'fruit_add', methods: ['POST'])]
     public function postFruit(
         Request $request,
-        SerializerInterface $serializer,
         ValidatorInterface $validator,
-        SluggerInterface $asciiSlugger
     ): JsonResponse {
-        $fruit = $serializer->deserialize(
-            $request->getContent(),
-            Fruit::class,
-            'json'
-        );
-
-        $alias = $asciiSlugger->slug($fruit->getName())
-            ->lower()
-            ->toString();
-        $fruit->setAlias($alias);
+        $jsonData = $request->getContent();
+        $fruit = $this->fruitService->prepareInput($jsonData);
 
         $violations = $validator->validate($fruit);
         if (count($violations) > 0) {
             throw new ValidationFailedException(null, $violations);
         }
 
-        $this->fruitRepository->add($fruit);
+        $this->fruitService->addFruit($fruit);
 
         return $this->json($fruit, Response::HTTP_CREATED);
     }
@@ -56,23 +42,15 @@ class FruitController extends AbstractController
     #[Route('/fruits/{page}', name: 'fruit_list', requirements: ['page' => '\d+'], methods: ['GET'])]
     public function getFruits(int $page = 1): JsonResponse
     {
-        $query = $this->fruitRepository->getQuery();
-        $pager = (new PaginationHelper($query))->paginate($page);
-        $fruits = $this->fruitRepository->getPaginatedResult($query, $pager->offset, $pager->limit);
+        $result = $this->fruitService->getPaginatedFruits($page);
 
-        return $this->json(
-            [
-                'fruits' => $fruits,
-                'pager' => $pager
-            ],
-            Response::HTTP_OK
-        );
+        return $this->json($result, Response::HTTP_OK);
     }
 
     #[Route('/fruits/{id}', name: 'fruit_delete', requirements: ['id' => '\d+'], methods: ['DELETE'])]
     public function deleteFruit(int $id): JsonResponse
     {
-        $fruit = $this->fruitRepository->find($id);
+        $fruit = $this->fruitService->findById($id);
 
         if ($fruit === null) {
             throw new NotFoundHttpException(
@@ -80,7 +58,7 @@ class FruitController extends AbstractController
             );
         }
 
-        $this->fruitRepository->remove($fruit);
+        $this->fruitService->removeFruit($fruit);
 
         return $this->json(null, Response::HTTP_NO_CONTENT);
     }

src/Service/FruitService.php
@@ -1,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Fruit;
+use App\Helper\PaginationHelper;
+use App\Repository\FruitRepository;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+final readonly class FruitService implements FruitServiceInterface
+{
+    public function __construct(
+        private FruitRepository $fruitRepository,
+        private SerializerInterface $serializer,
+        private SluggerInterface $asciiSlugger
+    ) {
+    }
+
+    public function addFruit(Fruit $fruit): void
+    {
+        $this->fruitRepository->add($fruit);
+    }
+
+    public function removeFruit(Fruit $fruit): void
+    {
+        $this->fruitRepository->remove($fruit);
+    }
+
+    public function findById(int $id): Fruit|null
+    {
+        return $this->fruitRepository->find($id);
+    }
+
+    public function prepareInput(string $jsonData): Fruit
+    {
+        $fruit = $this->serializer->deserialize(
+            $jsonData,
+            Fruit::class,
+            'json'
+        );
+
+        $alias = $this->asciiSlugger->slug($fruit->getName())
+            ->lower()
+            ->toString();
+        $fruit->setAlias($alias);
+
+        return $fruit;
+    }
+
+    public function getPaginatedFruits(int $page): array
+    {
+        $query = $this->fruitRepository->getQuery();
+        $pager = (new PaginationHelper($query))->paginate($page);
+        $fruits = $this->fruitRepository->getPaginatedResult($query, $pager->offset, $pager->limit);
+
+        return [
+            'fruits' => $fruits,
+            'pager' => $pager
+        ];
+    }
+}

src/Service/FruitServiceInterface.php
@@ -1,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Fruit;
+
+interface FruitServiceInterface
+{
+    public function addFruit(Fruit $fruit): void;
+
+    public function removeFruit(Fruit $fruit): void;
+
+    public function findById(int $id): Fruit|null;
+
+    public function prepareInput(string $jsonData): Fruit;
+
+    public function getPaginatedFruits(int $page): array;
+}

tarique-iqbal/fruits-vegetables-api

src/Controller/FruitController.php
@@ -4,51 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\Fruit;
-use App\Helper\PaginationHelper;
-use App\Repository\FruitRepository;
+use App\Service\FruitServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class FruitController extends AbstractController
+final class FruitController extends AbstractController
 {
     public function __construct(
-        private readonly FruitRepository $fruitRepository
+        private readonly FruitServiceInterface $fruitService,
     ) {
     }
 
     #[Route('/fruits', name: 'fruit_add', methods: ['POST'])]
     public function postFruit(
         Request $request,
-        SerializerInterface $serializer,
         ValidatorInterface $validator,
-        SluggerInterface $asciiSlugger
     ): JsonResponse {
-        $fruit = $serializer->deserialize(
-            $request->getContent(),
-            Fruit::class,
-            'json'
-        );
-
-        $alias = $asciiSlugger->slug($fruit->getName())
-            ->lower()
-            ->toString();
-        $fruit->setAlias($alias);
+        $jsonData = $request->getContent();
+        $fruit = $this->fruitService->deserializeInput($jsonData);
 
         $violations = $validator->validate($fruit);
         if (count($violations) > 0) {
             throw new ValidationFailedException(null, $violations);
         }
 
-        $this->fruitRepository->add($fruit);
+        $this->fruitService->addFruit($fruit);
 
         return $this->json($fruit, Response::HTTP_CREATED);
     }
@@ -56,23 +42,15 @@ class FruitController extends AbstractController
     #[Route('/fruits/{page}', name: 'fruit_list', requirements: ['page' => '\d+'], methods: ['GET'])]
     public function getFruits(int $page = 1): JsonResponse
     {
-        $query = $this->fruitRepository->getQuery();
-        $pager = (new PaginationHelper($query))->paginate($page);
-        $fruits = $this->fruitRepository->getPaginatedResult($query, $pager->offset, $pager->limit);
+        $result = $this->fruitService->getPaginatedFruits($page);
 
-        return $this->json(
-            [
-                'fruits' => $fruits,
-                'pager' => $pager
-            ],
-            Response::HTTP_OK
-        );
+        return $this->json($result, Response::HTTP_OK);
     }
 
     #[Route('/fruits/{id}', name: 'fruit_delete', requirements: ['id' => '\d+'], methods: ['DELETE'])]
     public function deleteFruit(int $id): JsonResponse
     {
-        $fruit = $this->fruitRepository->find($id);
+        $fruit = $this->fruitService->findById($id);
 
         if ($fruit === null) {
             throw new NotFoundHttpException(
@@ -80,7 +58,7 @@ class FruitController extends AbstractController
             );
         }
 
-        $this->fruitRepository->remove($fruit);
+        $this->fruitService->removeFruit($fruit);
 
         return $this->json(null, Response::HTTP_NO_CONTENT);
     }

src/Service/FruitService.php
@@ -1,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Fruit;
+use App\Helper\PaginationHelper;
+use App\Repository\FruitRepository;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+final readonly class FruitService implements FruitServiceInterface
+{
+    public function __construct(
+        private FruitRepository $fruitRepository,
+        private SerializerInterface $serializer,
+        private SluggerInterface $asciiSlugger
+    ) {
+    }
+
+    public function addFruit(Fruit $fruit): void
+    {
+        $this->fruitRepository->add($fruit);
+    }
+
+    public function removeFruit(Fruit $fruit): void
+    {
+        $this->fruitRepository->remove($fruit);
+    }
+
+    public function findById(int $id): Fruit|null
+    {
+        return $this->fruitRepository->find($id);
+    }
+
+    public function deserializeInput(string $jsonData): Fruit
+    {
+        $fruit = $this->serializer->deserialize(
+            $jsonData,
+            Fruit::class,
+            'json'
+        );
+
+        $alias = $this->asciiSlugger->slug($fruit->getName())
+            ->lower()
+            ->toString();
+        $fruit->setAlias($alias);
+
+        return $fruit;
+    }
+
+    public function getPaginatedFruits(int $page): array
+    {
+        $query = $this->fruitRepository->getQuery();
+        $pager = (new PaginationHelper($query))->paginate($page);
+        $fruits = $this->fruitRepository->getPaginatedResult($query, $pager->offset, $pager->limit);
+
+        return [
+            'fruits' => $fruits,
+            'pager' => $pager
+        ];
+    }
+}

src/Service/FruitServiceInterface.php
@@ -1,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Fruit;
+
+interface FruitServiceInterface
+{
+    public function addFruit(Fruit $fruit): void;
+
+    public function removeFruit(Fruit $fruit): void;
+
+    public function findById(int $id): Fruit|null;
+
+    public function deserializeInput(string $jsonData): Fruit;
+
+    public function getPaginatedFruits(int $page): array;
+}