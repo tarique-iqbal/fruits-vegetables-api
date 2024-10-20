fb1e2f4b0034c731d92f8170254180fe9a7018a6
Tarique Iqbal <live.iqbal@gmail.com>
2024-10-20 21:11:01 +0200

    Add AbstractApiController

---
+++ src/Controller/AbstractApiController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Dto\Request\FruitDto;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+abstract class AbstractApiController extends AbstractController
+{
+    public function __construct(
+        private readonly SerializerInterface $serializer,
+        private readonly SluggerInterface $asciiSlugger
+    ) {
+    }
+
+    abstract protected function getDtoClassName(): string;
+
+    protected function loadDto(Request $request): FruitDto
+    {
+        $dto = $this->serializer->deserialize(
+            $request->getContent(),
+            $this->getDtoClassName(),
+            'json'
+        );
+
+        $alias = $this->asciiSlugger->slug($dto->getName())
+            ->lower()
+            ->toString();
+        $dto->setAlias($alias);
+
+        return $dto;
+    }
+}
