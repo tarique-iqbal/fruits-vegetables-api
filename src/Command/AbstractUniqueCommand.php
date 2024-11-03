b340e68a0bc8c48f577b7f17db694e57e346f107
Tarique Iqbal <live.iqbal@gmail.com>
2024-11-03 11:56:04 +0100

    Add AbstractUniqueCommand

---
+++ src/Command/AbstractUniqueCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+abstract class AbstractUniqueCommand extends Command
+{
+    use LockableTrait;
+
+    public function __construct(private readonly ValidatorInterface $validator)
+    {
+        parent::__construct();
+    }
+
+    abstract protected function perform(InputInterface $input, OutputInterface $output): int;
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $command = $input->getArgument('command');
+
+        if ($this->lock($command) === false) {
+            $output->writeln(
+                sprintf('The command [%s] is already running in another process.', $command)
+            );
+
+            return self::FAILURE;
+        }
+
+        try {
+            return $this->perform($input, $output);
+        } finally {
+            $this->release();
+        }
+    }
+}
